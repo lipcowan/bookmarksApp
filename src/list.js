d805d76cf96b30fea70425389a9e758f066b4c9a
Lip Cowan <pcowan90@gmail.com>
2020-07-17 12:43:20 -0700

    adjusted css to make things more accessible

--- src/list.js
+++ src/list.js
@@ -12,14 +12,14 @@ const renderBookmark = function (bookmark) {
     <p>${bookmark.rating}</p>
     <p>${bookmark.desc}</p>  
     <a href=${bookmark.url}>visit site</a>
-    <button type='button' id='collapse'>-</button>
+    <button type='button' id='collapse' alt="collapse bookmark">-</button>
     <button type='button' id='js-edit-bookmark'>Edit Bookmark</button>
     <button type='button' id='js-delete-bookmark'>Delete Bookmark</button>
     </div>`;
   } else {
     return `<div class="css-bookmark" data-item-id="${bookmark.id}"><h2>${bookmark.title}</h2> 
       <p>${bookmark.rating}</p> 
-      <button type='button' id='expand'>+</button></div>`;
+      <button type='button' id='expand' alt="expand bookmark">+</button></div>`;
   }
 };
 
@@ -33,21 +33,36 @@ function renderNewBookmarkForm() {
             <label for="url">Website URL</label>
             <input type="url" name="url" id="js-bookmark-url-entry" placeholder="https://www.google.com" required>
             <label for="desc">Description</label>
-            <textarea name="desc" id="js-bookmark-description-entry" cols="20" required>"Please describe the site here"</textarea>
+            <textarea name="desc" id="js-bookmark-description-entry" cols="20" placeholder="Please describe the site here" required></textarea>
+            <div id="rating-area">
+            <label for="rating">Rating</label>
             <span class="star_ratings">
-                <input type="radio" id="rating-1" name="rating" value="1" />
-                <label for="rating-1">1</label>
-                <input type="radio" id="rating-2" name="rating" value="2" />
-                <label for="rating-2">2</label>
-                <input type="radio" id="rating-3" name="rating" value="3" />
-                <label for="rating-3">3</label>
-                <input type="radio" id="rating-4" name="rating" value="4" />
-                <label for="rating-4">4</label>
-                <input type="radio" id="rating-5" name="rating" value="5" />
-                <label for="rating-5">5</label>
+                <div>
+                  <label for="rating-1">1 Star</label>
+                  <input type="radio" id="rating-1" name="rating" value="1" />
+                </div>
+                <div>
+                  <label for="rating-2">2 Stars</label>
+                  <input type="radio" id="rating-2" name="rating" value="2" />
+                </div>
+                <div>
+                  <label for="rating-3">3 Stars</label>
+                  <input type="radio" id="rating-3" name="rating" value="3" />
+                </div>
+                <div>
+                  <label for="rating-4">4 Stars</label>
+                  <input type="radio" id="rating-4" name="rating" value="4" />
+                </div>
+                <div>
+                  <label for="rating-5">5 Stars</label>
+                  <input type="radio" id="rating-5" name="rating" value="5" />
+                </div>
             </span>
+            </div>
+            <div>
             <button type="submit">Create New Bookmark</button>
             <button type="button" id="cancel-button">Cancel</button>
+            </div>
         </fieldset>
     </form>`;
 }
